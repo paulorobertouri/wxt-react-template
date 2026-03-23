e12552371dd69f20bfa9543a2b710ef48eb236b8
Paulo <paulorobertouri@gmail.com>
2026-03-23 15:20:47 -0300

    ci: run vitest with coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,6 +10,14 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./tests/setupTests.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80
+    }
   },
   resolve: {
     alias: [{ find: '@', replacement: resolve(__dirname, './') }],
